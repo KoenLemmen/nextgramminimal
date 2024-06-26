dd2f62a481e7a89935a95c34fff0d13aca977835
Koen Lemmen <koenlemmen@metadeveloping.com>
2024-06-26 17:18:54 +0200

    Testing with next-static-utils

--- app/@modal/(.)photos/[id]/page.tsx
+++ app/@modal/(.)photos/[id]/page.tsx
@@ -1,9 +1,12 @@
+import { withDynamicParams } from "next-static-utils";
 import { Modal } from "./modal";
 
-export async function generateStaticParams() {
+let generateFunc = (): Promise<Record<string, string>[]> => {
   let photos = Array.from({ length: 6 }, (_, i) => i + 1);
-  return photos.map((id) => ({ id: String(id) }));
-}
+  return Promise.resolve(photos.map((id) => ({ id: String(id) })));
+};
+
+export const generateStaticParams = withDynamicParams(generateFunc);
 
 export default function PhotoModal({ params: { id: photoId } }: { params: { id: string } }) {
   return <Modal>{photoId}</Modal>;
